69888faa138b0d5bd83ae80307d2922c059e21ce
Archie <yangyxcn@gmail.com>
2015-06-15 09:28:41 +0800

    Fix history URL label view line breaks
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- APISound/HistoryViewCell.swift
+++ APISound/HistoryViewCell.swift
@@ -14,6 +14,7 @@ class HistoryViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        urlLabel.adjustsFontSizeToFitWidth = false
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
