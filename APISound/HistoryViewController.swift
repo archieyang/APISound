0c09bf70132dc0325df0380b18562bed75f4c5bc
Archie <yangyxcn@gmail.com>
2015-06-19 09:00:50 +0800

    Add check out history requests
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- APISound/HistoryViewController.swift
+++ APISound/HistoryViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import CoreData
 
-class HistoryViewController: UIViewController, UITableViewDataSource {
+class HistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
     var requestList = [APIRequest]()
+    
+    var mainViewController: MainViewController {
+        return (self.slideMenuController()?.mainViewController as! UINavigationController).topViewController as! MainViewController
+    }
 
     @IBOutlet weak var historyTableView: UITableView!
     
     override func viewDidLoad() {
         historyTableView.dataSource = self
+        historyTableView.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -40,17 +45,6 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     //MARK: Table View Data Source
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("historyViewCell", forIndexPath: indexPath) as! HistoryViewCell
@@ -60,6 +54,13 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return requestList.count
+    }
+    
+    //MARK: Table View Delegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.mainViewController.apiRequest = requestList[indexPath.row]
+        self.slideMenuController()?.closeLeft()
     }
 
 }
